ab69f7d665c68fc824c6f92bc50254fc55bf0658
Allen Liu <kamyu104@gmail.com>
2018-07-02 23:31:28 +0800

    Create bribe-the-prisoners.py

---
+++ Round 1C/bribe-the-prisoners.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2018 kamyu. All rights reserved.
+#
+# Google Code Jam 2009 Round 1C - Problem C. Bribe the Prisoners
+# https://code.google.com/codejam/contest/189252/dashboard#s=p2&a=2
+#
+# Time:  O(Q^3)
+# Space: O(Q^2)
+#
+
+def bribe_the_prisoners():
+    P, Q = map(int, raw_input().strip().split())
+    A = [0] + map(int, raw_input().strip().split()) + [P+1]
+    dp = [[float("inf") for _ in xrange(Q+2)] for _ in xrange(Q+1)]
+    for i in xrange(Q+1):
+        dp[i][i+1] = 0
+    for w in xrange(2, Q+2):
+        for i in xrange(Q+2-w):
+            j = i+w
+            min_coins = float("inf")
+            for k in xrange(i+1, j):
+                min_coins = min(min_coins, dp[i][k]+dp[k][j])
+            dp[i][j] = A[j]-A[i]-2 + min_coins
+    return dp[0][-1]
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, bribe_the_prisoners())
+
