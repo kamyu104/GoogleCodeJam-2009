bbb5adb8e4ab0e0b192c1fbb56e6a38c438593c1
kamyu <kamyu104@users.noreply.github.com>
2018-07-02 23:34:53 +0800

    Update bribe-the-prisoners.py

--- Round 1C/bribe-the-prisoners.py
+++ Round 1C/bribe-the-prisoners.py
@@ -15,8 +15,7 @@ def bribe_the_prisoners():
         dp[i][i+1] = 0
     for w in xrange(2, Q+2):
         for i in xrange(Q+2-w):
-            j = i+w
-            min_coins = float("inf")
+            j, min_coins = i+w, float("inf")
             for k in xrange(i+1, j):
                 min_coins = min(min_coins, dp[i][k]+dp[k][j])
             dp[i][j] = A[j]-A[i]-2 + min_coins
